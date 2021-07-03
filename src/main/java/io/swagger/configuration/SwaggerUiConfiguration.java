bb1ad20db67598a0b9a302cdd940707d4a3530c4
Ioram Sette <ioram.sette@crosswordcybersecurity.com>
2021-07-03 07:55:51 +0100

    chore: Put a license notice in each java file

--- src/main/java/io/swagger/configuration/SwaggerUiConfiguration.java
+++ src/main/java/io/swagger/configuration/SwaggerUiConfiguration.java
@@ -1,3 +1,18 @@
+//    This file is part of the Simple Universal Verifier (SUV). 
+// 
+//    SUV is free software: you can redistribute it and/or modify it under the 
+//    terms of the GNU Lesser General Public License as published by the Free 
+//    Software Foundation, either version 3 of the License, or (at your 
+//    option) any later version. 
+// 
+//    SUV is distributed in the hope that it will be useful,but WITHOUT ANY 
+//    WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+//    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public 
+//    License for more details. 
+// 
+//    You should have received a copy of the GNU Lesser General Public License 
+//    along with SUV.  If not, see <https://www.gnu.org/licenses/>. 
+
 package io.swagger.configuration;
 
 import org.springframework.context.annotation.Configuration;
