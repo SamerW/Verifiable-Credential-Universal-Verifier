5f0bd79ca2cb9b36aa72ba5f1553b36abd8f72ea
Ioram Sette <ioram.sette@crosswordcybersecurity.com>
2021-07-05 23:41:46 +0100

    chore: add Crossword Cybersecurity copyright, improve licensing notice.

--- src/main/java/io/identiproof/suv/model/Verifier.java
+++ src/main/java/io/identiproof/suv/model/Verifier.java
@@ -1,6 +1,8 @@
+//    Copyright (2021) Crossword Cybersecurity Plc
+//
 //    This file is part of the Simple Universal Verifier (SUV). 
 // 
-//    SUV is free software: you can redistribute it and/or modify it under the 
+//    SUV is licensed free of charge. You can redistribute it and/or modify it under the 
 //    terms of the GNU Lesser General Public License as published by the Free 
 //    Software Foundation, either version 3 of the License, or (at your 
 //    option) any later version. 
